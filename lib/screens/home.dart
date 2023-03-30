69d35888821881cb1cc53cc67cd76a2dd753c91d
Shantanu <shantanuwable2003@gmail.com>
2023-03-30 19:04:11 +0530

    Cleaned lint

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,8 +2,6 @@ import 'package:expenseplanner/screens/transaction_input.dart';
 import 'package:flutter/material.dart';
 
 import 'package:expenseplanner/models/transaction_data.dart';
-import 'package:expenseplanner/models/transaction.dart';
-
 import 'package:expenseplanner/widgets/expense_tracker.dart';
 import 'package:expenseplanner/widgets/transaction_widget.dart';
 
@@ -30,13 +28,12 @@ class _HomeState extends State<Home> {
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 Padding(
-                  padding: EdgeInsets.only(
+                  padding: const EdgeInsets.only(
                     right: 10.0,
                     top: 10.0,
                     ),
                   child: IconButton(
-                    color: Colors.black,
-                    icon: Icon(Icons.add, color: Colors.blue,),
+                    icon: const Icon(Icons.add, color: Colors.blue,),
                     onPressed: () {
                       Navigator.pushNamed(context, TransactionInput.id);
                     }
@@ -45,37 +42,35 @@ class _HomeState extends State<Home> {
               ],
             ),
             ExpenseTracker(amount: Provider.of<TransactionData>(context, listen: true).totalExpense()),
-            SizedBox(
+            const SizedBox(
                   height: 10,
             ),
             
            // add a scroll view for transactions
             Expanded(
-              child: Container(
-                child: Consumer<TransactionData>(
-                  builder: (context, transactionData, child)
-                  {
-                    return ListView.builder(
-                    itemBuilder: (BuildContext context, int index) {
-                      final ttransaction = transactionData.transactions[index];
-                      return Dismissible(
-                        key: ValueKey<TransactionWidget>(TransactionWidget(transaction: transactionData.transactions[index])),
-                        onDismissed: (DismissDirection direction) {
-                          setState(() {
-                            transactionData.deleteTransaction(ttransaction);
-                          });
-                        },
-                        background: Container(
-                          color: Colors.green,
-                        ),
-                        child: TransactionWidget(transaction: transactionData.transactions[index]),
-                      );
-                    },
-                    itemCount: transactionData.transactions.length,
-                  );
+              child: Consumer<TransactionData>(
+                builder: (context, transactionData, child)
+                {
+                  return ListView.builder(
+                  itemBuilder: (BuildContext context, int index) {
+                    final ttransaction = transactionData.transactions[index];
+                    return Dismissible(
+                      key: ValueKey<TransactionWidget>(TransactionWidget(transaction: transactionData.transactions[index])),
+                      onDismissed: (DismissDirection direction) {
+                        setState(() {
+                          transactionData.deleteTransaction(ttransaction);
+                        });
+                      },
+                      background: Container(
+                        color: Colors.green,
+                      ),
+                      child: TransactionWidget(transaction: transactionData.transactions[index]),
+                    );
                   },
-                )
-      ),
+                  itemCount: transactionData.transactions.length,
+                );
+                },
+              ),
     ),
           ],
         ),
@@ -83,21 +78,3 @@ class _HomeState extends State<Home> {
     );
   }
 }
-
-// Route _createRoute() {
-//   return PageRouteBuilder(
-//     pageBuilder: (context, animation, secondaryAnimation) => TransactionInput(),
-//     transitionsBuilder: (context, animation, secondaryAnimation, child) {
-//       const begin = Offset(0.0, 1.0);
-//       const end = Offset.zero;
-//       const curve = Curves.ease;
-
-//       var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-
-//       return SlideTransition(
-//         position: animation.drive(tween),
-//         child: child,
-//       );
-//     },
-//   );
-// }
